473ec52b785708e8ca208bb8bf5e6a8d886069a9
Xaxa <Prateek.Xaxa@kla-tencor.com>
2018-07-31 23:35:51 +0530

    first commit

---
+++ PubSub_Experiment/PubSub.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PubSub_Experiment
+{
+    partial class PubSub
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAction = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAction
+            // 
+            this.btnAction.Location = new System.Drawing.Point(12, 12);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(182, 56);
+            this.btnAction.TabIndex = 0;
+            this.btnAction.Text = "Action";
+            this.btnAction.UseVisualStyleBackColor = true;
+            // 
+            // PubSub
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 320);
+            this.Controls.Add(this.btnAction);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "PubSub";
+            this.Text = "PubSub [EXPERIMENT]";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAction;
+    }
+}
+
---
+++ PubSub_Experiment/PubSub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PubSub_Experiment
+{
+    public partial class PubSub : Form
+    {
+        public PubSub()
+        {
+            InitializeComponent();
+        }
+    }
+}
