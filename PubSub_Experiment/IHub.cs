49f033473f8201e2acaae3d1eeb4d9e7dd8ef132
Xaxa <Prateek.Xaxa@kla-tencor.com>
2018-07-31 23:39:25 +0530

    first commit

---
+++ PubSub_Experiment/IHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ICAN.SIC.Abstractions
+{
+    public interface IHub
+    {
+        string Name { get; }
+
+        List<string> GetAllHubNames();
+
+        void PublishDownwards<T>(T message, TimeSpan? delay = null) where T : IMessage;
+
+        void setMyParent(IHub hub);
+
+        void Publish<T>(T message, TimeSpan? delay = null) where T : IMessage;
+
+        void Subscribe<T>(Action<T> messageAction) where T : IMessage;
+
+        void Unsubscribe<T>(Action<T> messageAction) where T : IMessage;
+
+        void PassThrough(IHub hub);
+    }
+}
